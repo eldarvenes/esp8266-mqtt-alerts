f6dedcac19cc659d68561cb0bd7fcf5e1bd0c6b7
Eldar Venes <eldarvenes@gmail.com>
2024-05-20 22:14:48 +0200

    Create config.h

---
+++ config.h
@@ -0,0 +1,10 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+const char* ssid = "ssid";
+const char* password = "password";
+const char* mqtt_server = "xxxxxxxxxxxxxxxxxx.s1.eu.hivemq.cloud";
+const char* mqtt_username = "client_username";
+const char* mqtt_password = "client_password";
+
+#endif
